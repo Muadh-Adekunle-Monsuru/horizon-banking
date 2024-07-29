4b9500832ffd03ccd7bcb3214dce25c3e0fa639f
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-07-29 10:12:58 +0100

    removing env

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import { getLoggedInUser } from '@/lib/actions/user.actions';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 export default async function RootLayout({
@@ -9,5 +10,19 @@ export default async function RootLayout({
 	const user = await getLoggedInUser();
 	// if (!user) redirect('/sign-in');
 	console.log(user);
-	return <main>{children}</main>;
+	return (
+		<main className='flex min-h-screen w-full justify-between font-inter'>
+			{children}
+			<div className='auth-asset'>
+				<div>
+					<Image
+						src='/icons/auth-image.svg'
+						alt='auth image'
+						width={500}
+						height={400}
+					/>
+				</div>
+			</div>
+		</main>
+	);
 }
