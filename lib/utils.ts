6475c5df869602351a8497adcd62aa08ea8710e2
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-07-30 21:34:10 +0100

    fix deploy error 1

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-prototype-builtins */
-import { AccountTypes, CategoryCount, Transaction } from '@/types';
+import { AccountTypes } from '@/types';
 import { Banks } from '@prisma/client';
 import { type ClassValue, clsx } from 'clsx';
 import qs from 'query-string';
@@ -160,45 +160,6 @@ export function getAccountTypeColors(type: AccountTypes) {
 				subText: 'text-green-700',
 			};
 	}
-}
-
-export function countTransactionCategories(
-	transactions: Transaction[]
-): CategoryCount[] {
-	const categoryCounts: { [category: string]: number } = {};
-	let totalCount = 0;
-
-	// Iterate over each transaction
-	transactions &&
-		transactions.forEach((transaction) => {
-			// Extract the category from the transaction
-			const category = transaction.category;
-
-			// If the category exists in the categoryCounts object, increment its count
-			if (categoryCounts.hasOwnProperty(category)) {
-				categoryCounts[category]++;
-			} else {
-				// Otherwise, initialize the count to 1
-				categoryCounts[category] = 1;
-			}
-
-			// Increment total count
-			totalCount++;
-		});
-
-	// Convert the categoryCounts object to an array of objects
-	const aggregatedCategories: CategoryCount[] = Object.keys(categoryCounts).map(
-		(category) => ({
-			name: category,
-			count: categoryCounts[category],
-			totalCount,
-		})
-	);
-
-	// Sort the aggregatedCategories array by count in descending order
-	aggregatedCategories.sort((a, b) => b.count - a.count);
-
-	return aggregatedCategories;
 }
 
 export function extractCustomerIdFromUrl(url: string) {
